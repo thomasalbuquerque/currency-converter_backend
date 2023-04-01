0b7748581502bfd0c9ae7ea1aceea2185fcd4735
Thomas Albuquerque <tealbthomas@gmail.com>
2023-04-01 16:09:23 -0300

    Adicionados valores de configuração de banco de dados para o banco de dados remoto da Railway. Valores fixos (não em variáveis).

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,10 +1,27 @@
+const DATABASE_URL = process.env.DATABASE_URL;
+const PGPORT = process.env.PGPORT;
+const PGDATABASE = process.env.PGDATABASE;
+const PGHOST = process.env.PGHOST;
+const PGUSER = process.env.PGUSER;
+const PGPASSWORD = process.env.PGPASSWORD;
+
 module.exports = {
     development: {
         dialect: 'postgres',
-        host: 'localhost',
-        port: '5432',
-        database: 'currency_converter_development',
-        username: 'currency_converter',
-        password: 'currency_converter'
+        host: PGHOST,
+        port: PGPORT,
+        database: PGDATABASE,
+        username: PGUSER,
+        password: PGPASSWORD,
     }
 }
+// module.exports = {
+//     development: {
+//         dialect: 'postgres',
+//         host: 'containers-us-west-19.railway.app',
+//         port: 7269,
+//         database: 'railway',
+//         username: 'postgres',
+//         password: 'PVWTw6oMzKCaEQqtcGV6'
+//     }
+// }
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,32 @@
 import { Sequelize } from "sequelize";
-
+require("dotenv").config();
+const DATABASE_URL = process.env.DATABASE_URL;
+const PGPORT = Number(process.env.PGPORT);
+const PGDATABASE = process.env.PGDATABASE;
+const PGHOST = process.env.PGHOST;
+const PGUSER = process.env.PGUSER;
+const PGPASSWORD = process.env.PGPASSWORD;
+console.log(typeof (PGUSER))
+console.log(typeof (PGPASSWORD))
 export const sequelize = new Sequelize({
     dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'currency_converter_development',
-    username: 'currency_converter',
-    password: 'currency_converter',
+    host: PGHOST,
+    port: PGPORT,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
     define: {
         underscored: true
     }
 })
+// export const sequelize = new Sequelize({
+//     dialect: 'postgres',
+//     host: 'containers-us-west-19.railway.app',
+//     port: 7269,
+//     database: 'railway',
+//     username: 'postgres',
+//     password: 'PVWTw6oMzKCaEQqtcGV6',
+//     define: {
+//         underscored: true
+//     }
+// })
