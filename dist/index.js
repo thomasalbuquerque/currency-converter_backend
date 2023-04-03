2aebbd2cf65a19705af63a83860f8c2f11128241
Thomas Albuquerque <tealbthomas@gmail.com>
2023-04-03 15:13:23 -0300

    Adicionado express-session e connect-pg-simple em um middleware do servidor seguindo instruções do chatgpt.

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,21 @@ const cors_1 = __importDefault(require("cors"));
 const adminjs_1 = require("./adminjs");
 const database_1 = require("./database");
 const routes_1 = require("./routes");
-//test2
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+//const db = require('./config/database'); // Importe suas configurações de banco de dados do arquivo correspondente
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use(session({
+    store: new pgSession({
+        pool: database_1.sequelize,
+        tableName: 'session'
+    }),
+    secret: 'sua_chave_secreta_aqui',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 dias de validade para o cookie de sessão
+}));
 app.use(express_1.default.static('public'));
 app.use(express_1.default.json());
 app.use(routes_1.router);
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,25 @@ import cors from "cors"
 import { adminJs, adminJsRouter } from "./adminjs";
 import { sequelize } from "./database";
 import { router } from "./routes";
-//test2
+
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+//const db = require('./config/database'); // Importe suas configurações de banco de dados do arquivo correspondente
+
 const app = express()
 
 app.use(cors())
+
+app.use(session({
+    store: new pgSession({
+        pool: sequelize,
+        tableName: 'session'
+    }),
+    secret: 'sua_chave_secreta_aqui',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 dias de validade para o cookie de sessão
+}));
 
 app.use(express.static('public'))
 
