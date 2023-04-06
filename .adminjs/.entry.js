afd034eb6160a06956f44820c2580ea123e05351
Thomas Albuquerque <tealbthomas@gmail.com>
2023-04-06 20:10:08 -0300

    Apaguei dashboard customizado do adminjs.

--- .adminjs/.entry.js
+++
@@ -1,3 +0,0 @@
-AdminJS.UserComponents = {}
-import Component0 from '../src/adminjs/components/Dashboard'
-AdminJS.UserComponents.Component0 = Component0
--- dist/adminjs/index.js
+++ dist/adminjs/index.js
@@ -10,7 +10,6 @@ const sequelize_1 = __importDefault(require("@adminjs/sequelize"));
 const database_1 = require("../database");
 const resources_1 = require("./resources");
 const locale_1 = require("./locale");
-const dashboard_1 = require("./dashboard");
 const branding_1 = require("./branding");
 const authentication_1 = require("./authentication");
 adminjs_1.default.registerAdapter(sequelize_1.default);
@@ -19,7 +18,7 @@ exports.adminJs = new adminjs_1.default({
     rootPath: "/admin",
     resources: resources_1.adminJsResources,
     locale: locale_1.locale,
-    dashboard: dashboard_1.dashboardOptions,
+    //dashboard: dashboardOptions,
     branding: branding_1.brandingOptions
 });
 exports.adminJsRouter = express_1.default.buildAuthenticatedRouter(exports.adminJs, authentication_1.authenticationOptions, null, {
