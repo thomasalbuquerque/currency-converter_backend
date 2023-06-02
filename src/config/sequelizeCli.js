cdd2e74c07b451e93e53f874e37ea6cd71253da5
Thomas Albuquerque <tealbthomas@gmail.com>
2023-06-02 15:18:08 -0300

    Ajuste de importação de módulo no sequelizeCli. Após eu tentar rodar a migration para popular o banco no railway.

--- src/config/sequelizeCli.js
+++ src/config/sequelizeCli.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
-require('pg')
+require('pg');
+// const { DATABASE_URL } = require("./environment.js");
+
+const env = require("env-var");
+
+const DATABASE_URL = env.get("DATABASE_URL").required().asString()
 
 // const DATABASE_URL = process.env.DATABASE_URL;
 // const PGPORT = process.env.PGPORT;
@@ -7,9 +12,6 @@ require('pg')
 // const PGHOST = process.env.PGHOST;
 // const PGUSER = process.env.PGUSER;
 // const PGPASSWORD = process.env.PGPASSWORD;
-
-import { DATABASE_URL } from "../config/environment";
-
 
 // const PGPORT = 5432;
 // const PGDATABASE = 'currency_converter_development';
