c02a5253a7eee2c1a6715d926face1669d54b269
Thomas Albuquerque <tealbthomas@gmail.com>
2023-06-05 12:49:05 -0300

    Removidos comentários desnecessários.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,49 +1,8 @@
 import { Sequelize } from "sequelize";
 import { DATABASE_URL } from "../config/environment";
-// require("dotenv").config();
-// require('pg')
-// const DATABASE_URL = process.env.DATABASE_URL;
-// const PGPORT = Number(process.env.PGPORT);
-// const PGDATABASE = process.env.PGDATABASE;
-// const PGHOST = process.env.PGHOST;
-// const PGUSER = process.env.PGUSER;
-// const PGPASSWORD = process.env.PGPASSWORD;
-
-// const PGPORT = 5432;
-// const PGDATABASE = 'currency_converter_development';
-// const PGHOST = 'localhost';
-// const PGUSER = 'currency_converter';
-// const PGPASSWORD = 'currency_converter';
 
 export const sequelize = new Sequelize(DATABASE_URL, {
     define: {
         underscored: true
     }
 })
-
-// export const sequelize = new Sequelize({
-//     dialect: 'postgres',
-//     storage: "./session.sqlite",
-//     host: PGHOST,
-//     port: PGPORT,
-//     database: PGDATABASE,
-//     username: PGUSER,
-//     password: PGPASSWORD,
-//     define: {
-//         underscored: true
-//     }
-// })
-
-// import { Sequelize } from "sequelize";
-
-// export const sequelize = new Sequelize({
-//     dialect: 'postgres',
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'onebitflix_development',
-//     username: 'onebitflix',
-//     password: 'onebitflix',
-//     define: {
-//         underscored: true
-//     }
-// })
--- src/index.ts
+++ src/index.ts
@@ -30,68 +30,6 @@ app.use(
 );
 store.sync()
 
-// // load dependencies  
-// const session = require("express-session");
-
-// // initalize sequelize with session store
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-// // create database, ensure 'sqlite3' in your package.json 
-// const myStore = new SequelizeStore({
-//     db: sequelize,
-// });
-// // configure express 
-// app.use(
-//     session({
-//         secret: "keyboard cat",
-//         store: myStore,
-//         resave: false, // we support the touch method so per the express-session docs this should be set to false
-//         proxy: true, // if you do SSL outside of node. 
-//         cookie: { secure: true, maxAge: 420000 },
-//         saveUninitialized: false
-
-//     })
-// );
-// myStore.sync()
-
-// const cookieParser = require('cookie-parser');
-// const expressSession = require('express-session');
-// const SessionStore = require('express-session-sequelize')(expressSession.Store);
-
-// const Sequelize = require('sequelize');
-// const myDatabase = new Sequelize('database', 'username', 'password', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-// });
-// // create database, ensure 'sqlite3' in your package.json 
-
-// const sequelizeSessionStore = new SessionStore({
-//     db: sequelize,
-// });
-
-// app.use(cookieParser());
-// app.use(expressSession({
-//     secret: 'keep it secret, keep it safe.',
-//     store: sequelizeSessionStore,
-//     resave: false,
-//     saveUninitialized: false,
-//     proxy: true,
-//     cookie: { secure: true, maxAge: 420000 },
-// }));
-// continue as normal
-
-/*
-var cookieSession = require('cookie-session')
-
-app.use(cookieSession({
-    name: 'session',
-    keys: ['7483626'],
-
-    // Cookie Options
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
-*/
-
 app.use(router)
 
 // app.use(adminJs.options.rootPath, adminJsRouter)
